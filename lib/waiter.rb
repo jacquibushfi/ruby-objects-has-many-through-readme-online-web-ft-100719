0c1a6f87854808620a037239c3ce7d7c5db009d6
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 20:02:28 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -30,4 +30,11 @@ def best_tipper
   best_tipped_meal.customer
 end
 
+def worst_tipper
+  worse_tipped_meal = meals.min do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+  worst_tipped_meal.customer
+end
+
 end
